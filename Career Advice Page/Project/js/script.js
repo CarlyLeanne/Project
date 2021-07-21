091052d5b5f44932b5a3d303a9810d2a660659fa
E-Chapman <84228086+E-Chapman@users.noreply.github.com>
2021-07-21 15:31:19 +0100

    Update script.js

--- Career Advice Page/Project/js/script.js
+++ Career Advice Page/Project/js/script.js
@@ -1,23 +1,2 @@
-// Javascript goes here :)
-
-// const content = ['events-content, "join-us-content'];
-
-// function renderContent(id) {
-//   for (let i = 0; i < content.length; i++) {
-//     function renderContent(id) {
-//       for (let i = 0; i < content.length; i++) {
-//         if (id === content[i]) {
-//           document.getElementById(id).style.display = "block";
-//         }
-//       }
-//     }
-    
-//   }
-  
-// }
-// // const renderContent = () => {
-// //     console.log("render content");
-// // };
-
-// console.log(id);
-//   document.getElementById("events-content").style.display = "block";
+var email = window.prompt("Please enter your email here if you would like to join our community and receive our monthly newsletter:");
+alert("Thank you! Keep an eye on your inbox.");
